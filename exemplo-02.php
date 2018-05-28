041c3d49bbf1a6ba1d786abb6f82458168a794c2
Luiz Afonso Rampineli Bemfica <luiz.afonso@gmail.com>
2018-05-28 17:41:57 -0300

    mime types

--- exemplo-02.php
+++ exemplo-02.php
@@ -1,5 +1,23 @@
 <?php
 
+$filename = "logo.png";
 
+$base64 = base64_encode(file_get_contents($filename));
+
+$fileinfo = new finfo(FILEINFO_MIME_TYPE);
+
+$mimetype = $fileinfo->file($filename);
+
+#echo $mimetype;
+#echo "<br>";
+
+$base64encode = "data:".$mimetype.";base64,". $base64;
+
+
+#var_dump($base64);
 
 ?>
+
+<a href="<?=$base64encode?>" target="_blank">Link para a imagem</a>
+
+<img src="<?=$base64encode?>" />
