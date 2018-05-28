0f058580c745cf9c2e4546da6c8ba9976e7043f0
Luiz Afonso Rampineli Bemfica <luiz.afonso@gmail.com>
2018-05-28 17:17:30 -0300

    lendo arquivos

--- exemplo-01.php
+++ exemplo-01.php
@@ -1,11 +1,34 @@
 <?php
 
 $filename = "usuarios.csv";
+
 if(file_exists($filename)) {
 	$file = fopen($filename, "r"); #nao precisa colocar o +, pois neste ponto já sabemos que o arquivo existe. O + é só pra criar caso não exista.
-	$headers = fgets($file);
+	
 
-	var_dump($headers);
+	$headers = explode(",", fgets($file));
+
+
+	$data = array();
+	while($row = fgets($file)) {
+		
+		$rowData = explode(",", $row);
+		$linha = array();
+
+		for($i = 0; $i < count($headers); $i++) {
+
+			$linha[$headers[$i]] = $rowData[$i];
+
+
+		}
+
+		array_push($data, $linha);
+	}
+
+	fclose($file);
+
+	echo json_encode(($data));
+	
 }
 
 ?>
